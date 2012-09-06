9126af07d245c4d075cf890bb868eb245dac7ebb
John Myles White <jmw@johnmyleswhite.com>
2012-09-06 05:47:41 -0400

    Substantially revised SGD code w/ new features

--- sgd.jl
+++ sgd.jl
@@ -3,48 +3,83 @@
 abstract Model
 
 # NOTE: Models are assumed to have a .w "weights" field
-# that holds a vector of Float64 weight coefficients
+# that holds a vector of Float64 coefficients.
 
-predict(m::Model, x::Vector{Float64}, y::Float64) = dot(x,m.w)
-residual(m::Model, x::Vector{Float64}, y::Float64) = y-predict(m,x,y)
+predict(m::Model, x::Vector{Float64}) = dot(x, m.w)
+
+function predict(m::Model, x::Matrix{Float64})
+  n = size(x, 1)
+  p = size(x, 2)
+  predictions = zeros(n)
+  for i = 1:n
+    predictions[i] = predict(m, reshape(x[i, :], p))
+  end
+  return predictions
+end
+
+residuals(m::Model, x::Vector{Float64}, y::Float64) = y - predict(m, x)
+
+function residuals(m::Model, x::Matrix{Float64}, y::Vector{Float64})
+  n = size(x, 1)
+  p = size(x, 2)
+  # if n != length(y)
+  #   error("x and y must have the same length")
+  # end
+  res = zeros(n)
+  for i = 1:n
+    res[i] = y[i] - predict(m, reshape(x[i, :], p))
+  end
+  return res
+end
 
 # Update coefficients.
-# Select a step-size using grid-style line search.
-function update(m::Model, x::Vector{Float64}, y::Float64, log_state::Bool)
-  m.n += 1
-  current_cost = cost(m,x,y)
-  dw = gradient(m,x,y)
-
-  alphas = (10.0).^-(0:32)
-  push(alphas, 0.0)
-  alpha_star = alphas[1]
-  least_cost = current_cost
-
-  original_w = copy(m.w)
-
-  for alpha = alphas
-    # TODO: faster in-place update code
-    m.w = original_w + alpha * dw
-    proposed_cost = cost(m,x,y)
-
-    if proposed_cost <= least_cost
-      alpha_star = alpha
-      least_cost = proposed_cost
-    end
+# Modify to handle multiple examples at once.
+function update(m::Model,
+                x::Union(Vector{Float64}, Matrix{Float64}),
+                y::Union(Float64, Vector{Float64}),
+                learning_rate::Float64,
+                learning_rule::Symbol,
+                averaging::Bool)
+  # Increment the number of examples we've seen.
+  if typeof(x) == Vector{Float64}
+    m.n += 1
+  else
+    m.n += size(x, 1)
   end
 
-  m.w = original_w + alpha_star * dw
+  # Evaluate the gradient on these examples.
+  # This needs to be customized for vector vs. matrix inputs.
+  dw = -gradient(m, x, y)
 
+  # Use Scikits-Learning style learning rate schedules.
+  if learning_rule == :constant
+    alpha = learning_rate
+  elseif learning_rule == :optimal
+    alpha = 1.0 / (m.n + 1.0) # Make 1.0 configurable in the future.
+  elseif learning_rule == :inverse_scaling
+    alpha = learning_rate / m.n ^ 0.5 # Make 0.5 configurable in the future.
+  end
+
+  # Use ASGD to smooth weights over many examples.
+  # Only start averaging during second epoch.
+  if averaging && m.epoch > 1
+    new_w = m.w + alpha * dw
+    m.w = ((m.n - 1.0) / (m.n)) * m.w + (1.0 / m.n) * new_w
+  else
+    m.w = m.w + alpha * dw
+  end
+
+  # Stop processing if weights become corrupted.
   if any(isnan(m.w))
-    error("NaN's produced as coefficients")
-  end
-  if log_state
-    open("logs/model.tsv", "a") do f
-      println(f, join([current_cost, m.w], "\t"))
-    end
+    error("NaN's produced as coefficients during update step")
   end
 end
-update(m::Model, x::Vector{Float64}, y::Float64) = update(m,x,y,false)
+
+function update(m::Model,
+                x::Union(Vector{Float64}, Matrix{Float64}),
+                y::Union(Float64, Vector{Float64}))
+  update(m, x, y, 0.01, :constant, false)
+end
 
 # Parse fields from a row of the CSV file into floats.
 # Appends an intercept term.
@@ -57,43 +92,73 @@ function parse_fields(row::String)
   (x, y)
 end
 
-function fit(filename::String, m::Model)
-  function average(history)
-    sum(history) ./ length(history)
-  end
-  open(filename, "r") do data_file
-    history_length = 25_000
-    history = Array(Vector{Float64}, history_length)
-    i = 0
-    row = readline(data_file)
-    while length(row) != 0
-      i += 1
-      x, y = parse_fields(chomp(row))
-      update(m,x,y)
-      # Added weight averaging
-      index = i % history_length
-      if index == 0
-        index = history_length
+function fit(m::Model,
+             filename::String,
+             epochs::Int64,
+             minibatch_size::Int64,
+             learning_rate::Float64,
+             learning_rule::Symbol,
+             averaging::Bool,
+             logging::Bool,
+             trace::Bool,
+             interval_length::Int64)
+  for epoch = 1:epochs
+    m.epoch += 1
+    open(filename, "r") do data_file
+      while true
+        if minibatch_size == 1
+          row = readline(data_file)
+          if length(row) == 0
+            break
+          end
+          x, y = parse_fields(chomp(row))
+        else
+          row = readline(data_file)
+          if length(row) == 0
+            break
+          end
+          tmp_x, tmp_y = parse_fields(chomp(row))
+          x, y = (tmp_x', [tmp_y])
+          for i = 2:minibatch_size
+            row = readline(data_file)
+            if length(row) == 0
+              continue
+            end
+            tmp_x, tmp_y = parse_fields(chomp(row))
+            x = vcat(x, tmp_x')
+            push(y, tmp_y)
+          end
+        end
+        update(m, x, y, learning_rate, learning_rule, averaging)
+        if logging && m.n % interval_length == 0
+          println(join({"Iteration: $(m.n)", m.w}, "\t"))
+        end
+        if trace && m.n % interval_length == 0
+          # Need to add * ASCIIString, Float64 to Julia.
+          println("RMSE: " * string(rmse(m, filename)))
+        end
       end
-      history[index] = lm.w
-      if i > 2 * history_length && index == history_length
-        println("Average: $(average(history))")
-        lm.w = average(history)
-      end
-      # End weight averaging.
-      row = readline(data_file)
     end
   end
 end
 
-function rmse(filename::String, m::Model)
+function fit(m::Model, filename::String, epochs::Int64)
+  fit(m, filename, epochs, 1, 0.001, :constant, true, false, false, 0)
+end
+
+# Run for 1 epoch by default.
+function fit(m::Model, filename::String)
+  fit(m, filename, 1, 1, 0.001, :constant, true, false, false, 0)
+end
+
+function rmse(m::Model, filename::String)
   mse = 0.0
   open(filename, "r") do data_file
     i = 1
     row = readline(data_file)
     while length(row) != 0
       x, y = parse_fields(chomp(row))
-      se = residual(m,x,y)^2
+      se = residuals(m,x,y)^2
       mse = ((i-1.0)/i)*mse+(1.0/i)*se
       row = readline(data_file)
       i += 1
@@ -102,13 +167,16 @@ function rmse(filename::String, m::Model)
   sqrt(mse)
 end
 
-function predict(filename::String, m::Model)
+# Change to not use in-memory storage.
+# Default to printing predictions to STDOUT, but allow IOStream to be specified.
+# Allow forcing to use in-memory storage.
+function predict(m::Model, filename::String)
   predictions = Array(Float64, 0)
   open(filename, "r") do data_file
     row = readline(data_file)
     while length(row) != 0
       x, y = parse_fields(chomp(row))
-      push(predictions, predict(m,x,y))
+      push(predictions, predict(m,x))
       row = readline(data_file)
     end
   end
@@ -120,26 +188,70 @@ end
 type LinearModel <: Model
   w::Vector{Float64}
   n::Int64
+  epoch::Int64
 end
 
-LinearModel(w::Vector{Float64}) = LinearModel(w, 0)
+LinearModel(w::Vector{Float64}) = LinearModel(w, 0, 0)
 
 type RidgeModel <: Model
   w::Vector{Float64}
   lambda::Float64
   n::Int64
+  epoch::Int64
 end
 
-RidgeModel(w::Vector{Float64}, lambda::Float64) = RidgeModel(w, lambda, 0)
+RidgeModel(w::Vector{Float64}, lambda::Float64) = RidgeModel(w, lambda, 0, 0)
 
-cost(m::LinearModel, x::Vector{Float64}, y::Float64) = 0.5*residual(m,x,y)^2
+cost(m::LinearModel, x::Vector{Float64}, y::Float64) = 0.5*residuals(m,x,y)^2
 cost(m::RidgeModel, x::Vector{Float64}, y::Float64) =
     0.5*(residual(m,x,y)^2 + m.lambda*sum(m.w[2:end].^2))
+function cost(m::LinearModel, x::Matrix{Float64}, y::Vector{Float64})
+  n = size(x, 1)
+  p = size(x, 2)
+  total_cost = 0.0
+  for i = 1:n
+    total_cost += cost(m, reshape(x[i, :], p), y[i])
+  end
+  return total_cost
+end
+function cost(m::RidgeModel, x::Matrix{Float64}, y::Vector{Float64})
+  n = size(x, 1)
+  p = size(x, 2)
+  total_cost = 0.0
+  for i = 1:n
+    total_cost += 0.5*(residual(m, reshape(x[i, :], p), y))^2
+  end
+  total_cost += m.lambda * sum(m.w[2:end].^2)
+  return total_cost
+end
 
-gradient(m::LinearModel, x::Vector{Float64}, y::Float64) = residual(m,x,y)*x
+gradient(m::LinearModel, x::Vector{Float64}, y::Float64) = residuals(m,x,y)*(-x)
 function gradient(m::RidgeModel, x::Vector{Float64}, y::Float64)
-  r = residual(m,x,y)
-  dw = r*x - m.lambda*m.w
-  dw[1] = r*x[1] # Don't regularize intercept.
+  r = residuals(m,x,y)
+  dw = r*(-x) + m.lambda*m.w
+  dw[1] = r*(-x[1])
   return dw
+end
+function gradient(m::LinearModel, x::Matrix{Float64}, y::Vector{Float64})
+  n = size(x, 1)
+  p = size(x, 2)
+  summed_gradient = zeros(p)
+  for i = 1:n
+    summed_gradient += gradient(m, reshape(x[i, :], p), y[i])
+  end
+  return summed_gradient
+end
+function gradient(m::RidgeModel, x::Matrix{Float64}, y::Vector{Float64})
+  n = size(x, 1)
+  p = size(x, 2)
+  summed_gradient = zeros(p)
+  for i = 1:n
+    local_x = reshape(x[i, :], p)
+    r = residuals(m, local_x, y[i])
+    summed_gradient += r * (-local_x)
+  end
+  for j = 2:p
+    summed_gradient[j] += m.lambda * m.w[j]
+  end
+  return summed_gradient
 end
--- toy_example.jl
+++ toy_example.jl
@@ -1,9 +1,29 @@
 # Load libraries.
 load("sgd.jl")
 
-# Fit a simple linear model.
+# Set up a linear model.
 lm = LinearModel(zeros(2))
-fit("data/toy.csv", lm)
+
+# Fit a simple linear model with one-pass through the data.
+fit(lm, "data/toy.csv")
 
 # Look at the results a bit.
 lm
+
+# Make four more passes.
+fit(lm, "data/toy.csv", 4)
+
+# Make five more passes.
+fit(lm, "data/toy.csv", 5)
+
+# Make ninety more passes.
+fit(lm, "data/toy.csv", 90)
+
+# Exploit all options.
+fit(lm, "data/toy.csv", 1, 1, 0.01, :constant, true, true, true, 10)
+
+# Exploit all options.
+fit(lm, "data/toy.csv", 1, 1, 0.01, :constant, true, true, false, 1)
+
+# Exploit all options.
+fit(lm, "data/toy.csv", 1, 25, 0.01, :constant, true, true, true, 25)
